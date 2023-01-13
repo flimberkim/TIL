1d8812e66e39210adf28a4880da136482f72d56c
flimberkim <113500922+flimberkim@users.noreply.github.com>
2023-01-13 21:14:14 +0900

    [Silver IV] Title: 수 찾기, Time: 620 ms, Memory: 42476 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,61 +5,32 @@ import java.util.*;
 
 public class Main {
 
-    public static int binarySearch(int[] arr, int find){ //이진탐색 메서드 구현
-
-        int first = 0; //배열의 첫번째 인덱스
-        int last = arr.length - 1; //배열의 마지막 인덱스
-
-        while(first <= last){
-
-            int mid = (first + last) / 2; // 중간위치의 인덱스
-
-            if(find < arr[mid]){
-                last = mid -1;
-            }
-            else if(find > arr[mid]){
-                first = mid + 1;
-            }
-            else return mid;
-        }
-        return -1;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int[] arr1;
-        int num = 0;
+        StringTokenizer st;
+        int firstSize = Integer.parseInt(input.readLine());
+        int[] arr1 = new int[firstSize];
         int temp = 0;
 
-
-        num = Integer.parseInt(input.readLine()); //처음 입력 받을 수의 개수
-        arr1 = new int[num];
         st = new StringTokenizer(input.readLine());
-        for (int j = 0; j < num; j++) { //처음 입력 받은 수들을 arr1에 저장
-            arr1[j] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < firstSize; i++){
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr1);
 
-
-        num = Integer.parseInt(input.readLine()); //두번째 입력 받을 수의 개수
+        int secondSize = Integer.parseInt(input.readLine());
 
         st = new StringTokenizer(input.readLine());
-        for (int j = 0; j < num; j++) { //두번째 입력 받은 수들을 바로 비교해서 처리
+        for(int i = 0; i < secondSize; i++){
             temp = Integer.parseInt(st.nextToken());
-            if(binarySearch(arr1, temp) >= 0){
-                sb.append(1);
-                sb.append("\n");
+
+            if(Arrays.binarySearch(arr1, temp) >= 0){
+                sb.append(1).append("\n");
             }
-            else{
-                sb.append(0);
-                sb.append("\n");
-            }
+            else sb.append(0).append("\n");
         }
 
-        System.out.println(sb); //최종결과인 sb 출력
-
+        System.out.println(sb);
     }
 }
