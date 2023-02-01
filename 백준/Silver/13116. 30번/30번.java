dad1f23ef7dcc02c2f15ee0a331c46c1173b3d7e
flimberkim <113500922+flimberkim@users.noreply.github.com>
2023-02-01 16:34:45 +0900

    [Silver IV] Title: 30번, Time: 340 ms, Memory: 34620 KB -BaekjoonHub

--- 백준/Silver/13116. 30번/30번.java
+++ 백준/Silver/13116. 30번/30번.java
@@ -12,19 +12,19 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int temp = 0;
-
-        for(int i = 0; i < num; i++){
+        
+        for(int i = 0; i < num; i++){ //값을 입력받음
             st = new StringTokenizer(input.readLine());
             int min = Integer.parseInt(st.nextToken());
             int max = Integer.parseInt(st.nextToken());
 
-            if(min > max){
+            if(min > max){ //둘 중 작은 수를 min에 저장
                 temp = min;
                 min = max;
                 max = temp;
             }
             
-            while(min != max){
+            while(min != max){ //min값과 max값을 비교하면서 2로 나눠가고, 같아지는 순간 케이스 종료
                 if(min > max){
                     min /= 2;
                     if(min == max) break;
