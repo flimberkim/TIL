6cbff30ff636f7eee98c5b62969dde6cad0cf729
flimberkim <113500922+flimberkim@users.noreply.github.com>
2023-02-28 19:35:35 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb;
+        
+        sb = new StringBuilder(input.readLine());
+        
+        sb.append("??!");
+        System.out.println(sb);
+
+    }
+
+}
