ac9b45462574574f42fc501168d0c52014bc8fa7
flimberkim <113500922+flimberkim@users.noreply.github.com>
2023-10-02 22:00:01 +0900

    [Silver II] Title: 최대 힙, Time: 340 ms, Memory: 26308 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,3 +1,7 @@
+//시간복잡도 : O(NlogN), N은 입력받는 횟수
+//아이디어 : 값을 입력받고, 값에 따라 큐에 추가하거나, 최대값을 출력한다.
+//자료구조 : 우선순위 큐
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,24 +10,24 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(input.readLine());
-        PriorityQueue<Integer> testset = new PriorityQueue<>(Collections.reverseOrder()); //최대 힙을 반환하는 큐 생성
-        ArrayList<Integer> result = new ArrayList<>(); //결과값만 모아놓을 배열
-        int num = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNum = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < testCase; i++){ //testCase만큼 반복
-            num = Integer.parseInt(input.readLine()); //매 케이스 마다의 연산이 무엇인지 입력받음
-            if(num == 0){ //0을 입력받은 경우 
-                if(testset.isEmpty()) result.add(0); //큐가 비어있으면 0을 반환해서 배열에 저장
-                else{
-                    result.add(testset.poll()); //큐가 비어있지 않으면 최대값을 반환하고 큐에서 제거
+        for(int i = 0; i < caseNum; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) {
+                if (que.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(que.poll()).append("\n");
                 }
             }
-            testset.add(num); //입력받은 수가 0이 아니면, 그 값을 큐에 저장
+            else {
+                que.add(num);
+            }
         }
-        for(int i : result){ //결과값을 모아 놓은 배열을 출력
-            System.out.println(i);
-        }
+        System.out.println(sb.toString());
     }
 }
