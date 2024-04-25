266adae46c1e046a2fc382d9e0e8c00dbea72ce8
flimberkim <113500922+flimberkim@users.noreply.github.com>
2024-04-25 19:51:55 +0900

    [Bronze V] Title: X보다 작은 수, Time: 164 ms, Memory: 15264 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -23,7 +23,6 @@ public class Main {
                 sb.append(num).append(" ");
             }
         }
-        sb.deleteCharAt(sb.length()-1);
 
         System.out.println(sb.toString());
     }
