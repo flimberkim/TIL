41123e96c24f764bf8251e5c3616a2e733cfff0f
flimberkim <113500922+flimberkim@users.noreply.github.com>
2024-10-11 15:32:21 +0900

    [level 1] Title: 달리기 경주, Time: 714.40 ms, Memory: 390 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -4,23 +4,29 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         Map<String, Integer> memo = new HashMap<>();
         
+        String player1;
+        String player2;
+        int player1Index = 0;
+        int player2Index = 0;
+        int temp;
+        
+        //맵에 선수와 등수를 기록
         for(int i = 0; i < players.length; i++){
             memo.put(players[i], i);
         }
         
         for(int i = 0; i < callings.length; i++){
-            int index = memo.get(callings[i]);
-            
-            //추월당할 선수 찾아놓기
-            String temp = players[index-1];
-            players[index-1] = players[index];
-            players[index] = temp;
-            
-            //추월당한 선수의 인덱스를 갱신
-            memo.put(players[index], index);
-            memo.put(players[index-1], index-1);
+            player1 = callings[i];
+            player1Index = memo.get(player1);
+            player2Index = player1Index - 1;
+            player2 = players[player2Index];
+            temp = player1Index;
+            memo.put(player1, player2Index);
+            memo.put(player2, temp);
+            players[player1Index] = player2;
+            players[player2Index] = player1;
         }
-
+          
         return players;
     }
 }
