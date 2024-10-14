e9ce1f04e0677fd98dfa0958759cd7f7c7b99867
flimberkim <113500922+flimberkim@users.noreply.github.com>
2024-10-14 19:51:59 +0900

    [level 1] Title: 추억 점수, Time: 2.76 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,27 +1,35 @@
 import java.util.*;
 
+/*
+아이디어 : 먼저 인물과 인물에 대한 그리움 값을 Map으로 저장해두고, photo에 대해 반복문을 돌면서 각 사진의 그리움 점수 총 값을 저장한다.
+시간복잡도 : O(N), N은 photo 배열의 행*열 
+자료구조 : 배열
+*/
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        Map<String, Integer> memo = new HashMap<>();
-        int sum = 0;
-
+        
+        int temp = 0;
+        
+        Map<String, Integer> record = new HashMap<>();
+        
         for(int i = 0; i < name.length; i++){
-            memo.put(name[i], yearning[i]);
+            record.put(name[i], yearning[i]);
         }
-
+        
         for(int i = 0; i < photo.length; i++){
-            String[] temp = photo[i];
-
-            for(int j = 0; j < temp.length; j++){
-                if(memo.containsKey(temp[j])){
-                    sum += memo.get(temp[j]);
-                }
+            temp = 0;
+            
+            for(int j = 0; j < photo[i].length; j++){
+                temp += record.getOrDefault(photo[i][j], 0);                
             }
-            answer[i] = sum;
-            sum = 0;
+            
+            answer[i] = temp;
+            
         }
         
         return answer;
+        
     }
 }
